27958dce7402ef3f8da01203189a2f595a6ebb25
shihao-hub <2958017271@qq.com>
2024-09-09 01:09:35 +0800

    2024-09-09-01:09:34

--- git_push.py
+++ git_push.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     with open(BAT_FILE_PATH, "r", encoding="utf-8") as file:
         content = file.read()
 
+    # 2024-09-09：这模式匹配我每次用的时候都非常非常不熟练，这到底是为什么呢？
     today = time.strftime('%Y-%m-%d-%H:%M:%S', time.localtime())
     content = re.compile(r"(git commit -m )(.+)").sub(lambda m: f"{m.group(1)}\"{today}\"", content)
 
