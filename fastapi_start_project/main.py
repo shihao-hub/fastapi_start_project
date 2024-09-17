2e9da51cb2dd9e92925166b1ccec2b2a84011080
shihao-hub <2958017271@qq.com>
2024-09-17 13:39:39 +0800

    2024-09-17-13:39:38

--- fastapi_start_project/main.py
+++ fastapi_start_project/main.py
@@ -23,11 +23,16 @@ app = FastAPI(**dict(
 # 挂载静态资源文件目录
 app.mount("/static", settings.staticfiles, name="static")
 
+# (TD)!: 自定义 docs，为了保证模板所需资源可以从本地获取 p53 页
+
+
 # 注册 urls （简单模仿一下 Django 的文件目录的，这种简单的方式可以让项目结构清晰明了，很舒服）
 apps.authentication.urls.register(app)
 apps.index.urls.register(app)
 apps.test.urls.register(app)
 
 if __name__ == '__main__':
+    # http://127.0.0.1:8888/docs#
+    # http://127.0.0.1:8888/redoc#
     app_model_name = os.path.basename(__file__)[:-3]
     uvicorn.run(f"{app_model_name}:app", host="127.0.0.1", port=8888, reload=True)
--- git_push.bat
+++ git_push.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "2024-09-10-01:58:39"
+git commit -m "2024-09-17-13:39:38"
 git push origin master:main
